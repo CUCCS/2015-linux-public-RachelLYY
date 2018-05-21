51a2f502db44c4e9135759566ea7d033084e9d78
RachelLYY <778527910@qq.com>
2018-05-22 00:57:44 +0800

    作业提交

--- lab6/code/main.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-expect /home/shell/ftp.sh
-expect /home/shell/dhcp_server.sh
-expect /home/shell/nfs_server.sh
-expect /home/shell/samba.sh
--- lab6/code/samba.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/expect
-set timeout 1
-set PASS rachel
-set USER root
-spawn sudo ssh -l $USER 192.168.137.94
-expect "*#*" { send "apt-get install smbclient\r" }
-expect "Y/n" { send "Y\r"}
-
-
